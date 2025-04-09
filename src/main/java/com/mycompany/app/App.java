f699aefa0a7ffb89b2b1fbc447e8d0ecb3fb6d33
cg ng <ngchyeguan@gmail.com>
2025-04-09 14:44:32 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "I love Jenkins";
+    //private static final String MESSAGE = "dummy string";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println("Maven is awesome!");
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
