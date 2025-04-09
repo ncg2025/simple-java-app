37ceb5c7f0f4b0959a4dce320c4b372c6ca53bc1
tyhean <tyhean@gmail.com>
2025-04-09 16:17:26 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Maven is awesome!");
+        System.out.println("Modify something");
     }
 
     public String getMessage() {
