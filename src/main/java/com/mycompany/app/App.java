be995e44a3c2f26bb8a0b78a97f978aaa2dacba9
tyhean <tyhean@gmail.com>
2025-04-09 16:36:49 +0800

    Revert "make it fail"
    This reverts commit 2d73da21f735943094ba737801e51e7ed70cc55a.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,8 @@ package com.mycompany.app;
  */
 public class App {
 
-    //private static final String MESSAGE = "I love Jenkins";
-    private static final String MESSAGE = "dummy string";
+    private static final String MESSAGE = "I love Jenkins";
+    //private static final String MESSAGE = "dummy string";
 
     public App() {}
 
